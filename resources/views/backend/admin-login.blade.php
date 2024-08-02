32985b2ad4c6def63f2bb7a13258064cee35fb3f
khaleel raweh <khaleelvisa@gmail.com>
2024-08-02 18:19:24 +0300

    save all

--- resources/views/backend/admin-login.blade.php
+++ resources/views/backend/admin-login.blade.php
@@ -1,92 +1,82 @@
-@extends('layouts.admin-auth')
-<style>
-    @media (min-width:767px) {
-        .login-card {
-            width: 40%;
-        }
-    }
-</style>
+@extends('layouts.master2')
+
+@section('css')
+    <!-- Sidemenu-respoansive-tabs css -->
+    <link href="{{ URL::asset('assets/plugins/sidemenu-responsive-tabs/css/sidemenu-responsive-tabs.css') }}"
+        rel="stylesheet">
+@endsection
 @section('content')
-    <div class="out d-flex justify-content-center align-items-center w-100" style="height: 100vh">
-        <div class="card login-card">
-            <div class="card-body">
-
-                <div class="text-center mt-4">
-                    <div class="mb-3">
-                        <a href="{{ route('admin.index') }}" class="auth-logo">
-                        </a>
+    <div class="container-fluid">
+        <div class="row no-gutter">
+            <!-- The image half -->
+            <div class="col-md-6 col-lg-6 col-xl-7 d-none d-md-flex bg-primary-transparent">
+                <div class="row wd-100p mx-auto text-center">
+                    <div class="col-md-12 col-lg-12 col-xl-12 my-auto mx-auto wd-100p">
+                        <img src="{{ URL::asset('assets/img/media/login.png') }}"
+                            class="my-auto ht-xl-80p wd-md-100p wd-xl-80p mx-auto" alt="logo">
                     </div>
                 </div>
-
-                <h4 class="text-muted text-center font-size-18"><b>تسجيل الدخول</b></h4>
-
-                <div class="p-3">
-                    <form class="form-horizontal mt-3" action="{{ route('login') }}" method="POST" class="user">
-                        @csrf
-                        <div class="form-group mb-3 row">
-                            <div class="col-12">
-                                <div class="form-group">
-                                    {{-- <label for="email">Email Address</label> --}}
-                                    <input type="text" name="email"
-                                        class="form-control form-control--sm js_email_fe rounded-pill @if ($errors->has('email') || $errors->has('username')) has-error @endif"
-                                        placeholder="ادخل اسم المستخدم او البريد الالكتروي" value="{{ old('email') }}">
-                                    @if ($errors->has('email') || $errors->has('username'))
-                                        <span class="help">{{ $errors->first('email') }}
-                                            {{ $errors->first('username') }}</span>
-                                    @endif
-                                </div>
-
-                            </div>
-                        </div>
-
-
-                        <div class="form-group mb-3 row">
-                            <div class="col-12">
-                                <div class="form-group">
-                                    <input type="password" name="password" required autocomplete="current-password"
-                                        class="form-control form-control--sm js_email_fe rounded-pill"
-                                        placeholder="ادخل كلمة المرور" />
-
-                                    @error('password')
-                                        <span class="invalid-feedback text-danger" role="alert">
-                                            <strong>{{ $message }}</strong>
-                                        </span>
-                                    @enderror
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="form-group mb-3 row">
-                            <div class="col-12">
-                                <div class="custom-control custom-checkbox">
-                                    <input type="checkbox" class="custom-control-input" id="remember" name="remember"
-                                        {{ old('remember') ? 'checked' : '' }}>
-                                    <label class="form-label ms-1" for="remember">تذكرني في المرة المقبلة</label>
-                                </div>
-                            </div>
-                        </div>
-
-
-                        <div class="form-group mb-3 text-center row mt-3 pt-1">
-                            <div class="col-12">
-                                <button
-                                    class="btn btn-info w-100 waves-effect waves-light form-control--sm js_email_fe rounded-pill"
-                                    type="submit">تسجيل الدخول</button>
-                            </div>
-                        </div>
-
-
-                    </form>
-                    <div class="form-group mb-0 row mt-2">
-                        <div class="col-sm-7 mt-3">
-                            <a href="{{ route('admin.recover-password') }}" class="text-muted"><i class="mdi mdi-lock"></i>
-                                نسيت كلمة المرور؟</a>
-                        </div>
-                    </div>
-                </div>
-                <!-- end -->
             </div>
-            <!-- end cardbody -->
+            <!-- The content half -->
+            <div class="col-md-6 col-lg-6 col-xl-5 bg-white">
+                <div class="login d-flex align-items-center py-2">
+                    <!-- Demo content-->
+                    <div class="container p-0">
+                        <div class="row">
+                            <div class="col-md-10 col-lg-10 col-xl-9 mx-auto">
+                                <div class="card-sigin">
+                                    <div class="mb-5 d-flex"> <a href="{{ url('/' . ($page = 'index')) }}"><img
+                                                src="{{ URL::asset('assets/img/brand/favicon.png') }}"
+                                                class="sign-favicon ht-40" alt="logo"></a>
+                                        <h1 class="main-logo1 ml-1 mr-0 my-auto tx-28">Ad<span>el</span>xpress</h1>
+                                    </div>
+                                    <div class="card-sigin">
+                                        <div class="main-signup-header">
+                                            <h2>مرحبا مجددا !</h2>
+                                            <h5 class="font-weight-semibold mb-4">الرجاء تسجيل الدخول من اجل المتابعة</h5>
+                                            <form action="{{ route('login') }}" method="POST">
+                                                @csrf
+                                                <div class="form-group">
+                                                    <label for="email">إسم المستخدم او البريد الإلكتروني</label>
+
+                                                    <input class="form-control" name="email" value="{{ old('email') }}"
+                                                        placeholder="ادخل اسم المستخدم او البريد الالكتروي" type="text">
+                                                    @if ($errors->has('email') || $errors->has('username'))
+                                                        <span class="help">{{ $errors->first('email') }}
+                                                            {{ $errors->first('username') }}</span>
+                                                    @endif
+                                                </div>
+
+                                                <div class="form-group">
+                                                    <label>كلمة المرور</label>
+                                                    <input class="form-control" name="password" required
+                                                        autocomplete="current-password" placeholder="ادخل كلمة المرور"
+                                                        type="password">
+                                                    @error('password')
+                                                        <span class="invalid-feedback text-danger" role="alert">
+                                                            <strong>{{ $message }}</strong>
+                                                        </span>
+                                                    @enderror
+                                                </div>
+                                                <button class="btn btn-main-primary btn-block" type="submit">تسجيل
+                                                    الدخول</button>
+
+
+                                            </form>
+                                            <div class="main-signin-footer mt-5">
+                                                <p><a href="{{ route('admin.recover-password') }}"> نسيت كلمة المرور؟</a>
+                                                </p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div><!-- End -->
+                </div>
+            </div><!-- End -->
         </div>
     </div>
+@endsection
+@section('js')
 @endsection
