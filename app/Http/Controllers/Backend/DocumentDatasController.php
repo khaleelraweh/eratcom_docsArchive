4a9168c268110fc29667e39fc4a6c09e0e3b703b
khaleel raweh <khaleelvisa@gmail.com>
2024-08-06 16:32:33 +0300

    save all

--- app/Http/Controllers/Backend/DocumentDatasController.php
+++ app/Http/Controllers/Backend/DocumentDatasController.php
@@ -3,9 +3,19 @@
 namespace App\Http\Controllers\Backend;
 
 use App\Http\Controllers\Controller;
+use App\Models\DocumentData;
 use Illuminate\Http\Request;
 
 class DocumentDatasController extends Controller
 {
-    //
+    public function index()
+    {
+        if (!auth()->user()->ability('admin', 'manage_document_datas , show_document_datas')) {
+            return redirect('admin/index');
+        }
+
+        $documenDatas = DocumentData::all();
+
+        return view('backend.document_datas.index', compact('documenDatas'));
+    }
 }
