4d153b6c782e9609c3bca1ddf754770d493832b1
khaleel raweh <khaleelvisa@gmail.com>
2024-08-06 16:27:24 +0300

    save all

--- app/Models/PageVariable.php
+++ app/Models/PageVariable.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class PageVariable extends Model
 {
@@ -15,5 +16,10 @@ class PageVariable extends Model
     public function pageGroup(): BelongsTo
     {
         return $this->belongsTo(PageGroup::class);
+    }
+
+    public function documentDatas(): HasMany
+    {
+        return $this->hasMany(DocumentData::class);
     }
 }
