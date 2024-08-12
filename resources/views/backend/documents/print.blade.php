20a25f67e57c8f53aae10a4cf1f3521c8bc06aa7
khaleel raweh <khaleelvisa@gmail.com>
2024-08-12 23:05:45 +0300

    save all

--- resources/views/backend/documents/print.blade.php
+++ resources/views/backend/documents/print.blade.php
@@ -24,85 +24,19 @@
     @livewireStyles
 @endsection
 
-@section('page-header')
-    <!-- breadcrumb -->
-    <div class="breadcrumb-header justify-content-between">
-        <div class="my-auto">
-            <div class="d-flex">
-                <h4 class="content-title mb-0 my-auto">
-                    <a href="{{ route('admin.documents.index') }}">{{ __('panel.manage_documents') }}</a>
 
-                </h4>
-                <span class="text-muted mt-1 tx-13 mr-2 mb-0">
-                    /
-                    {{ __('panel.show_documents') }}
-                </span>
-            </div>
-        </div>
-        <div class="d-flex my-xl-auto right-content">
-
-
-            <div class="pr-1 mb-3 mb-xl-0">
-                {{-- <button type="button" class="btn btn-warning  btn-icon ml-2"><i class="mdi mdi-refresh"></i></button> --}}
-                <a href="{{ route('admin.document_templates.create') }}" class="btn btn-warning  btn-icon ml-2">
-                    <i class="mdi mdi-refresh"></i>
-                </a>
-            </div>
-
-
-        </div>
-    </div>
-    <!-- breadcrumb -->
-@endsection
 
 @section('content')
     <div class="row row-sm ">
         <div class="col-xl-12">
+
             <div class="card">
-                <div class="card-header pb-0">
-                    <div class="d-flex justify-content-between">
-                        <h4 class="card-title mg-b-0">
-                            <i class="fa fa-plus-square me-3 " style="font-size: 20px;"></i>
-                            {{ __('panel.show_documents') }}
-                        </h4>
-                        <i class="mdi mdi-dots-horizontal text-gray"></i>
-                    </div>
-                </div>
                 <div class="card-body">
-                    <div class="table-responsive">
-                        <table class="table">
-                            <tr>
-                                <th>{{ __('panel.document_name') }}</th>
-                                <td>{{ $document->doc_name }}</td>
-                                <th>{{ __('panel.document_number') }}</th>
-                                <td>{{ $document->doc_no ?? '-' }}</td>
-                            </tr>
-                            <tr>
-                                <th>{{ __('panel.document_type') }}</th>
-                                <td>{{ $document->doc_type() }}</td>
-                                <th>{{ __('panel.created_at') }}</th>
-                                <td>{{ $document->created_at }}</td>
-                            </tr>
-                            <tr>
-                                <th>{{ __('panel.document_status') }}</th>
-                                <td>{{ $document->doc_status() }}</td>
-                                <th>{{ __('panel.document_file') }}</th>
-                                <td>{{ $document->doc_file ?? '-' }} </td>
-                            </tr>
-
-                        </table>
-
-                        <h3>{{ __('panel.document_text') }}</h3>
-                        <div class="card">
-                            <div class="card-body">
-                                {!! $document->doc_content !!}
-                            </div>
-                        </div>
-                    </div>
-
-
+                    {!! $document->doc_content !!}
                 </div>
             </div>
+
+
         </div>
 
     </div>
