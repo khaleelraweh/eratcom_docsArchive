3d130fa5d7251710b74d1881fb333af560337a3d
khaleel raweh <khaleelvisa@gmail.com>
2024-08-13 00:00:12 +0300

    save all

--- app/Http/Controllers/Backend/DocumentsController.php
+++ app/Http/Controllers/Backend/DocumentsController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use App\Models\Document;
 use App\Models\DocumentTemplate;
 use Illuminate\Http\Request;
+use PDF;
 
 class DocumentsController extends Controller
 {
