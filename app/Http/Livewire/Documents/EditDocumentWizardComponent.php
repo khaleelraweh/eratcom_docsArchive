b9cd449966c361229a4fccb7a7dbc17be1381584
khaleel raweh <khaleelvisa@gmail.com>
2024-08-13 17:08:41 +0300

    save all

--- app/Http/Livewire/Documents/EditDocumentWizardComponent.php
+++ app/Http/Livewire/Documents/EditDocumentWizardComponent.php
@@ -2,20 +2,61 @@
 
 namespace App\Http\Livewire\Documents;
 
+use App\Models\Document;
+use App\Models\DocumentCategory;
+use App\Models\DocumentTemplate;
+use App\Models\DocumentType;
+use Jantinnerezo\LivewireAlert\LivewireAlert;
 use Livewire\Component;
 
 class EditDocumentWizardComponent extends Component
 {
+    use LivewireAlert;
+
+    //global variables
+    public $currentStep = 1;
+    public $totalSteps = 4;
 
     public $document_id;
+    public $document;
+
+    public $document_categories;
+    public $document_category_id;
+
+    public $document_types = [];
+    public $document_type_id;
+
+    public $document_templates = [];
+    public $document_template_id;
+
+    public $chosen_template;
+
+
+
 
     public function mount($document_id)
     {
         $this->document_id = $document_id;
+
+        $this->document_categories  = DocumentCategory::whereStatus(true)->get();
+        $this->document_types       = $this->document_category_id != '' ? DocumentType::whereStatus(true)->whereDocumentCategoryId($this->document_category_id)->get() : [];
+        $this->document_templates       = $this->document_type_id != '' ? DocumentTemplate::whereStatus(true)->whereDocumentTypeId($this->document_type_id)->get() : [];
+        $this->document = Document::find($this->document_id);
     }
 
     public function render()
     {
-        return view('livewire.documents.edit-document-wizard-component');
+
+        $this->document_categories  = DocumentCategory::whereStatus(true)->get();
+        $this->document_types       = $this->document_category_id != '' ? DocumentType::whereStatus(true)->whereDocumentCategoryId($this->document_category_id)->get() : [];
+        $this->document_templates       = $this->document_type_id != '' ? DocumentTemplate::whereStatus(true)->whereDocumentTypeId($this->document_type_id)->get() : [];
+        $this->document = Document::find($this->document_id);
+
+        return view('livewire.documents.edit-document-wizard-component', [
+            'document_categories'   => $this->document_categories,
+            'document_types'        => $this->document_types,
+            'document_templates'    => $this->document_templates,
+            'document'              => $this->document,
+        ]);
     }
 }
--- resources/views/livewire/documents/edit-document-wizard-component.blade.php
+++ resources/views/livewire/documents/edit-document-wizard-component.blade.php
@@ -1,3 +1,96 @@
 <div>
-    we are in edit document
+    <link rel="stylesheet" href="{{ asset('assets/css/mywizard.css') }}">
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Cairo&display=swap" rel="stylesheet">
+
+
+    <style>
+        body {
+            font-family: 'DejaVu Sans', sans-serif;
+            direction: rtl;
+            text-align: right;
+        }
+
+        fieldset {
+            border: 1px solid #ddd;
+            border-radius: 4px;
+            margin-bottom: 20px;
+            padding: 15px;
+        }
+
+        legend {
+            font-size: 1.2em;
+            color: #333;
+            font-weight: bold;
+            width: fit-content;
+            padding: 0 0.7rem;
+        }
+
+        label {
+            display: block;
+            margin: 10px 0 5px;
+            color: #555;
+        }
+    </style>
+
+    <div class="mywizard">
+        <div class="steps clearfix">
+            <ul role="tablist">
+                <li role="tab" wire:click="directMoveToStep(1)"
+                    class="first {{ $currentStep == 1 ? 'current' : '' }}" aria-disabled="false" aria-selected="true">
+                    <a id="wizard1-t-0" href="#wizard1-h-0" aria-controls="wizard1-p-0">
+                        <span class="current-info audible">current step:
+                        </span>
+                        <span class="number">1</span>
+                        <span class="title">
+                            {{ __('panel.document_template_data') }}
+                        </span>
+                    </a>
+                </li>
+                @if ($document_template)
+                    @if (count($document_template->documentPages) > 0)
+                        @foreach ($document_template->documentPages as $key => $documentPage)
+                            <li role="tab" wire:click="directMoveToStep({{ $key + 2 }})"
+                                class="disabled {{ $currentStep == $key + 2 ? 'current' : '' }}" aria-disabled="true">
+                                <a id="wizard1-t-{{ $key + 2 }}" href="#wizard1-h-1"
+                                    aria-controls="wizard1-p-{{ $key + 2 }}">
+                                    <span class="number">{{ $key + 2 }}</span>
+                                    <span class="title">
+
+                                        {{-- {!! Str::limit($documentPage->doc_page_name, 10, ' ...') !!} --}}
+
+                                        {!! Str::words($documentPage->doc_page_name, 3, ' ...') !!}
+
+                                        {{-- {{ $documentPage->doc_page_name }} --}}
+                                    </span>
+                                </a>
+                            </li>
+                        @endforeach
+                    @endif
+
+                @endif
+
+                @if ($document_template)
+                    @if (count($document_template->documentPages) > 0)
+                        <li role="tab" wire:click="directMoveToStep({{ $totalSteps }})"
+                            class="first {{ $currentStep == $totalSteps ? 'current' : '' }}" aria-disabled="false"
+                            aria-selected="true">
+                            <a id="wizard1-t-0" href="#wizard1-h-0" aria-controls="wizard1-p-0">
+                                <span class="current-info audible">current step:
+                                </span>
+                                <span class="number">{{ $totalSteps }}</span>
+                                <span class="title">
+                                    {{ __('panel.document_review') }}
+                                </span>
+                            </a>
+                        </li>
+                    @endif
+                @endif
+
+            </ul>
+        </div>
+    </div>
+
 </div>
