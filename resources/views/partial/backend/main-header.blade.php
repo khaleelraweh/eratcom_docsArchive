9066d635785fd9cd2075a3ce6233e34854ee3ab5
khaleel raweh <khaleelvisa@gmail.com>
2024-08-14 23:49:57 +0300

    save all

--- resources/views/partial/backend/main-header.blade.php
+++ resources/views/partial/backend/main-header.blade.php
@@ -118,6 +118,15 @@
                         </div>
                     </form>
                 </div>
+                <style>
+                    .pulse-danger {
+                        top: 0px;
+                        right: 7px;
+                        width: 12px;
+                        height: 12px;
+                        border: 2px solid #fff;
+                    }
+                </style>
                 <div class="dropdown nav-item main-header-message ">
                     <a class="new nav-link" href="#"><svg xmlns="http://www.w3.org/2000/svg"
                             class="header-icon-svgs" viewBox="0 0 24 24" fill="none" stroke="currentColor"
@@ -126,7 +135,9 @@
                             <path d="M4 4h16c1.1 0 2 .9 2 2v12c0 1.1-.9 2-2 2H4c-1.1 0-2-.9-2-2V6c0-1.1.9-2 2-2z">
                             </path>
                             <polyline points="22,6 12,13 2,6"></polyline>
-                        </svg><span class=" pulse-danger"></span></a>
+                        </svg>
+
+                        <span class=" pulse-danger"></span></a>
                     <div class="dropdown-menu">
                         <div class="menu-header-content bg-primary text-right">
                             <div class="d-flex">
@@ -209,6 +220,17 @@
                         </div>
                     </div>
                 </div>
+                <style>
+                    .pulse {
+                        box-shadow: 0 0 2px #fff;
+                        border-radius: 50%;
+                        top: 0px;
+                        right: 7px;
+                        width: 12px;
+                        height: 12px;
+                        border: 2px solid #fff;
+                    }
+                </style>
                 <div class="dropdown nav-item main-header-notification">
                     <a class="new nav-link" href="#">
                         <svg xmlns="http://www.w3.org/2000/svg" class="header-icon-svgs" viewBox="0 0 24 24"
@@ -216,6 +238,7 @@
                             stroke-linejoin="round" class="feather feather-bell">
                             <path d="M18 8A6 6 0 0 0 6 8c0 7-3 9-3 9h18s-3-2-3-9"></path>
                             <path d="M13.73 21a2 2 0 0 1-3.46 0"></path>
+
                         </svg><span class=" pulse"></span></a>
                     <div class="dropdown-menu">
                         <div class="menu-header-content bg-primary text-right">
